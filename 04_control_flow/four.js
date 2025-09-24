ce2fc739cafc973e423f4b08b0d28aaa78eed669
Snehatiwarie <144262379+Snehatiwarie@users.noreply.github.com>
2025-09-24 09:05:01 +0530

    loops.js
    04_control

--- 04_control_flow/four.js
+++ 04_control_flow/four.js
@@ -1,1 +1,50 @@
-const myObject = 
+const myObject =  {
+    js : "javascript",
+    py : "python",
+    rb : "ruby"
+}
+
+for (const key in myObject ) {
+   // console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+
+const programming = ["js","py","rb"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+// programming.forEach( function (val){
+//     console.log(val);
+// })
+
+// programming.forEach( (item) => {
+//     console.log(item);
+// })
+
+//or
+
+// function printme(item) {
+//     console.log(item);
+// }
+
+// programming.forEach(printme);
+
+// for objects inside arrays
+const myCoding = [
+    {
+        languageName : "js",
+        languageFileName  : "javascript"
+    },
+    {
+        languageName : "py",
+        languageFileName  : "python"
+    },
+    {
+        languageName : "cpp",
+        languageFileName  : "c++"
+    },
+]
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
---
+++ 05_basics/six.js
@@ -0,0 +1,9 @@
+const myNums = [1,2,3,4,5,6,78,9]
+// const newNums = myNums.filter ( (num) => {
+//       return num > 4;
+// })
+
+//OR ANOTTHER SYNATX WE CAN USE
+
+const newNums = myNums.filter ( (num) => ( num > 4))
+console.log(newNums);
